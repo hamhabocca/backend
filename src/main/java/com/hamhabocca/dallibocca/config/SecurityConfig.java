15a98c5762d528c123dd3e0149c6fa889190888b
Yuhyun Kim <christykim47@gmail.com>
2023-04-13 18:22:33 +0900

    [feat] Security
    swagger에 암호가 자꾸 걸려 현재 전부 주석 밑 disable 처리 해놓은 상태임

---
+++ src/main/java/com/hamhabocca/dallibocca/config/SecurityConfig.java
@@ -0,0 +1,113 @@
+//package com.hamhabocca.dallibocca.config;
+//
+////import com.hamhabocca.dallibocca.jwt.JwtAccessDeniedHandler;
+////import com.hamhabocca.dallibocca.jwt.JwtAuthenticationEntryPoint;
+////import com.hamhabocca.dallibocca.jwt.TokenProvider;
+//import java.util.Arrays;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+////    private final TokenProvider tokenProvider;
+////    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+////    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+//
+////    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint
+////            , JwtAccessDeniedHandler jwtAccessDeniedHandler){
+////        this.tokenProvider = tokenProvider;
+////        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+////        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+////    }
+//
+//	/* jwt 설정을 피하기 위한 생성자 */
+//	public SecurityConfig() {}
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//
+//        web
+//                // 외부에서 이미지 파일에 접근 가능 하도록 설정
+//                .ignoring()
+//                .antMatchers("/productimgs/**");
+//
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+////        http
+////                // CSRF 설정 Disable
+////                .csrf().disable()
+////                // exception handling
+////                .exceptionHandling()
+////                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+////                .accessDeniedHandler(jwtAccessDeniedHandler)
+////
+////                // 시큐리티는 기본적으로 세션을 사용하지만 API 서버에선 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+////                .and()
+////                .sessionManagement()
+////                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+////
+////                    // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+////                    .and()
+////                    .authorizeRequests()
+////                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+////                    .antMatchers("/auth/**").permitAll()
+////                    .antMatchers("/api/v1/products/**").permitAll()
+////                    .antMatchers("/api/v1/reviews/**").permitAll()
+////                    .antMatchers("/api/**").hasAnyRole("USER", "ADMIN")  // 나머지 API 는 전부 인증 필요
+////                .and()
+////                .cors()
+////                .and()
+////                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+////                .apply(new JwtSecurityConfig(tokenProvider));
+//
+//        http.authorizeRequests()
+//            .antMatchers(
+//                "/api/v1/**",
+//                "/swagger-ui.html",
+//                "/swagger/**",
+//                "/v1/api-docs",
+//                "/swagger-resources/**",
+//                "/webjars/**"
+//            ).permitAll()
+//            .anyRequest().authenticated()
+//            .and()
+//            .csrf().disable()
+//            .formLogin()
+//            .and()
+//            .httpBasic();
+//    }
+//
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource(){
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        // 로컬 React에서 오는 요청은 CORS 허용해준다.
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000" ));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
+//        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Content-Type", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//
+//}
