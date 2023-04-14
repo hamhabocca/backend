6b65d2c5f9b396e59c38da5fe27ec09c4be1ba05
filson1205 <sondongfil1205@gmail.com>
2023-04-14 10:09:12 +0900

    [fix] Repository 수정
    쿼리문 제거

--- src/main/java/com/hamhabocca/dallibocca/comment/repository/CommentRepository.java
+++ src/main/java/com/hamhabocca/dallibocca/comment/repository/CommentRepository.java
@@ -6,8 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-
-	@Query(value = "SELECT A.MEMBER_ID, A.NICKNAME, A.COMMENT, A.COMMENT_WRITE_DATE FROM COMMENT A ORDER BY A.MEMBER_ID ASC",
-		nativeQuery = true)
 	List<Comment> findAllComment();
 }
