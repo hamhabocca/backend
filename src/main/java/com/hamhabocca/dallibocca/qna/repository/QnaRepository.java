9ca60e85f602ef7a93eae47f79d1468aa9b44908
filson1205 <sondongfil1205@gmail.com>
2023-04-14 10:08:24 +0900

    [fix] Repository 수정
    쿼리문 제거

--- src/main/java/com/hamhabocca/dallibocca/qna/repository/QnaRepository.java
+++ src/main/java/com/hamhabocca/dallibocca/qna/repository/QnaRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface QnaRepository extends JpaRepository<Qna, Integer> {
-
-    @Query(value = "SELECT A.QNA_ID, A.QNA_DETAIL, A.QNA_TITLE, A.QNA_WRITE_DATE, A.QNA_WRITER, A.QNA_CATEGORY FROM QNA A ORDER BY A.QNA_ID ASC",
-            nativeQuery = true)
     List<Qna> findAllQna();
     List<Qna> findByQnaTitleContaining(String qnaTitle);
 
